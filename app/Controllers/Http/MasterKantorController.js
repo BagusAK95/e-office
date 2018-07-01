59d5346e69eaac11d695011437a24b8abbd33cb7
BagusAK95 <bagus.ak95@gmail.com>
2018-07-01 12:18:19 +0700

    Fix Tree

--- app/Controllers/Http/MasterKantorController.js
+++ app/Controllers/Http/MasterKantorController.js
@@ -7,22 +7,40 @@ class MasterKantorController {
         const user = await auth.getUser()
         const parentLokasi = user.kode_lokasi.toString().replace(/\d{5}$/g, '00000')
 
-        let data = {}
+        let data = {
+            id: null,
+            text: null,
+            nodes: []
+        }
+
         const lokasi1 = await MasterKantor.query().where('kdlokasi', Number(parentLokasi)).first()
         if (lokasi1) {
-            data[lokasi1.kdlokasi + '|' + lokasi1.nmlokasi.trim()] = {}
+            data.id = lokasi1.kdlokasi
+            data.text = lokasi1.nmlokasi
 
             const lokasi2 = await MasterKantor.query().where('kdparent', lokasi1.kdlokasi)
             for (let i = 0; i < lokasi2.length; i++) {
-                data[lokasi1.kdlokasi + '|' + lokasi1.nmlokasi.trim()][lokasi2[i].kdlokasi + '|' + lokasi2[i].nmlokasi.trim()] = {}
+                data.nodes.push({
+                    id: lokasi2[i].kdlokasi,
+                    text: lokasi2[i].nmlokasi,
+                    nodes: []
+                })
 
                 const lokasi3 = await MasterKantor.query().where('kdparent', lokasi2[i].kdlokasi)
                 for (let j = 0; j < lokasi3.length; j++) {
-                    data[lokasi1.kdlokasi + '|' + lokasi1.nmlokasi.trim()][lokasi2[i].kdlokasi + '|' + lokasi2[i].nmlokasi.trim()][lokasi3[j].kdlokasi + '|' + lokasi3[j].nmlokasi.trim()] = {}
+                    data.nodes[i].nodes.push({
+                        id: lokasi3[j].kdlokasi,
+                        text: lokasi3[j].nmlokasi,
+                        nodes: []
+                    })
 
                     const lokasi4 = await MasterKantor.query().where('kdparent', lokasi3[j].kdlokasi)
                     for (let k = 0; k < lokasi4.length; k++) {
-                        data[lokasi1.kdlokasi + '|' + lokasi1.nmlokasi.trim()][lokasi2[i].kdlokasi + '|' + lokasi2[i].nmlokasi.trim()][lokasi3[j].kdlokasi + '|' + lokasi3[j].nmlokasi.trim()][lokasi4[k].kdlokasi + '|' + lokasi4[k].nmlokasi.trim()] = {}
+                        data.nodes[i].nodes[j].nodes.push({
+                            id: lokasi4[k].kdlokasi,
+                            text: lokasi4[k].nmlokasi,
+                            nodes: []
+                        })
                     }
                 }
             }
