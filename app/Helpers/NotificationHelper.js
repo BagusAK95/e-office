460cf370472d84edd9cba64b2e9253879f006f4a
BagusAK95 <bagus.ak95@gmail.com>
2018-07-20 16:14:00 +0700

    Setup New Firebase

--- app/Helpers/NotificationHelper.js
+++ app/Helpers/NotificationHelper.js
@@ -1,7 +1,8 @@
-const Request = use('request')
+const Request = require('request')
 const AsyncLoop = require('node-async-loop')
 const Notifikasi = use('App/Models/Notifikasi')
 const Login = use('App/Models/Login')
+const Env = use('Env')
 
 const NotificationHelper = {
     send: function(nip_pengirim, nip_penerima, isi, url) {
@@ -17,18 +18,25 @@ const NotificationHelper = {
                 if (insert) {
                     const user = await Login.query().where('nip', nip).first()
                     if (user) {
+                        
+
                         for (let i = 1; i <= 2; i++) {
                             let firebase = ((i == 1) ? user.firebase_web : user.firebase_app)
 
-                            const form = {
-                                notification: {
-                                  title: 'e-office',
-                                  body: isi
+                            Request.post({
+                                url: 'https://fcm.googleapis.com/fcm/send',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': 'key=' + Env.get('FIREBASE_SERVER_KEY')
                                 },
-                                to: firebase
-                            }
-
-                            Request.post({url: 'https://fcm.googleapis.com/fcm/send', form: form})
+                                form : {
+                                    notification: {
+                                      title: 'e-office',
+                                      body: isi
+                                    },
+                                    to: firebase
+                                }
+                            })
                         }
 
                         next()
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/5.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': '1050031306341'
+  'messagingSenderId': '1039512108885'
 });
 
 var messaging = firebase.messaging();
