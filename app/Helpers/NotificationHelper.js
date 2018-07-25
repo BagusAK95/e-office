5020e4e13f735cca5fa6fb4a98aeb7be35f6f9e6
BagusAK95 <bagus.ak95@gmail.com>
2018-07-25 23:59:04 +0700

    Fix

--- app/Helpers/NotificationHelper.js
+++ app/Helpers/NotificationHelper.js
@@ -6,55 +6,54 @@ const Env = use('Env')
 
 const NotificationHelper = {
     send: function(pengirim, penerima, isi, url) {
-        const locNull = pengirim.indexOf('')
-        if (locNull != -1) {
-            delete pengirim[locNull]    
-        }
-
         AsyncLoop(penerima, async (nip, next) => {
             try {
-                const insert = await Notifikasi.create({
-                    nip_pengirim: pengirim[0],
-                    nip_penerima: nip,
-                    isi: pengirim[1].trim() + ' ' + isi.trim(),
-                    url: url
-                })
-
-                if (insert) {
-                    const user = await Login.query().where('nip', nip).first()
-                    if (user) {
-                        for (let i = 1; i <= 2; i++) {
-                            const firebase = ((i == 1) ? user.firebase_web : user.firebase_app)
-                            if (firebase) {
-                                const response = ((i == 1) ? 'response_web' : 'response_app')
-
-                                Request.post({
-                                    url: 'https://fcm.googleapis.com/fcm/send',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'Authorization': 'key=' + Env.get('FIREBASE_SERVER_KEY')
-                                    },
-                                    body : JSON.stringify({
-                                        notification: {
-                                            title: pengirim[1].trim(),
-                                            body: isi,
-                                            click_action: '/notifikasi/' + insert.id
+                if (nip === "") {
+                    next()
+                } else {
+                    const insert = await Notifikasi.create({
+                        nip_pengirim: pengirim[0],
+                        nip_penerima: nip,
+                        isi: pengirim[1].trim() + ' ' + isi.trim(),
+                        url: url
+                    })
+    
+                    if (insert) {
+                        const user = await Login.query().where('nip', nip).first()
+                        if (user) {
+                            for (let i = 1; i <= 2; i++) {
+                                const firebase = ((i == 1) ? user.firebase_web : user.firebase_app)
+                                if (firebase) {
+                                    const response = ((i == 1) ? 'response_web' : 'response_app')
+    
+                                    Request.post({
+                                        url: 'https://fcm.googleapis.com/fcm/send',
+                                        headers: {
+                                            'Content-Type': 'application/json',
+                                            'Authorization': 'key=' + Env.get('FIREBASE_SERVER_KEY')
                                         },
-                                        to: firebase
+                                        body : JSON.stringify({
+                                            notification: {
+                                                title: pengirim[1].trim(),
+                                                body: isi,
+                                                click_action: '/notifikasi/' + insert.id
+                                            },
+                                            to: firebase
+                                        })
+                                    }, async (err, resp, body) => {
+                                        (err) ? insert[response] = err : insert[response] = body
+                                        await insert.save()
                                     })
-                                }, async (err, resp, body) => {
-                                    (err) ? insert[response] = err : insert[response] = body
-                                    await insert.save()
-                                })
+                                }
                             }
+    
+                            next()
+                        } else {
+                            next()
                         }
-
-                        next()
                     } else {
                         next()
                     }
-                } else {
-                    next()
                 }
             } catch (error) {
                 next()
