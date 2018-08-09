0d9427aa4a13fcc46461092d588859ca8cedf7ef
BagusAK95 <bagus.ak95@gmail.com>
2018-08-09 21:06:46 +0700

    Fix Complete User

--- app/Commands/CompleteUserData.js
+++ app/Commands/CompleteUserData.js
@@ -26,17 +26,17 @@ class CompleteUserData extends Command {
       dataPegawai = JSON.parse(JSON.stringify(dataPegawai))
       if (dataPegawai.data.length > 0) {
         AsyncLoop(dataPegawai.data, async (pegawai, next) => {
+          let arrNamaLengkap = [pegawai.gelar_depan, pegawai.nama, pegawai.gelar_belakang]
+          arrNamaLengkap = arrNamaLengkap.filter(function(n){ return n != null })
+
+          let instansi = pegawai.kode_lokasi
+          if (instansi) instansi = instansi.toString().replace(/\d{5}$/g, '00000')
+
+          let jabatan = pegawai.nama_jabatan
+          if (!jabatan) jabatan = ''
+
           const exist = await Login.find(pegawai.nip)
           if (!exist) {
-              let arrNamaLengkap = [pegawai.gelar_depan, pegawai.nama, pegawai.gelar_belakang]
-              arrNamaLengkap = arrNamaLengkap.filter(function(n){ return n != null })
-
-              let instansi = pegawai.kode_lokasi
-              if (instansi) instansi = instansi.toString().replace(/\d{5}$/g, '00000')
-
-              let jabatan = pegawai.nama_jabatan
-              if (!jabatan) jabatan = ''
-
               const data = {
                 nip: pegawai.nip,
                 instansi: instansi,
@@ -48,15 +48,26 @@ class CompleteUserData extends Command {
                 golongan: pegawai.golongan,
                 password: await Hash.make(pegawai.nip),
                 level: 4,
-                akses: '',
+                akses: 'disposisi',
                 status: 1,
                 keyword: jabatan.concat(' | ', arrNamaLengkap.join(' '))
               }
 
               const insert = await Login.create(data)
-              console.log(insert.nama_lengkap + ' -> Success.')
+
+              console.log(insert.nama_lengkap + ' -> Created.')
             } else {
-              console.log(exist.nama_lengkap + ' -> Exist.')
+              exist.instansi = instansi
+              exist.nama_lengkap = arrNamaLengkap.join(' ')
+              exist.kode_lokasi = pegawai.kode_lokasi
+              exist.kode_jabatan = pegawai.kode_jabatan
+              exist.nama_jabatan = pegawai.nama_jabatan
+              exist.kode_eselon = pegawai.kode_eselon
+              exist.golongan = pegawai.golongan
+              exist.keyword = jabatan.concat(' | ', arrNamaLengkap.join(' '))
+              await exist.save()
+
+              console.log(exist.nama_lengkap + ' -> Updated.')
             }
 
             next()
@@ -75,24 +86,33 @@ class CompleteUserData extends Command {
     try {
       const masterKantor = await MasterKantor.query().where('kdparent', null)
       AsyncLoop(masterKantor, async(kantor, next) => {
-        const data = {
-          nip: kantor.kdlokasi,
-          instansi: kantor.kdlokasi,
-          nama_lengkap: kantor.nmlokasi,
-          kode_lokasi: null,
-          kode_jabatan: null,
-          nama_jabatan: null,
-          kode_eselon: null,
-          golongan: null,
-          password: await Hash.make(kantor.kdlokasi.toString()),
-          level: 1,
-          akses: 'administrator',
-          status: 1,
-          keyword: ''
-        }
+        const exist = await Login.find(kantor.kdlokasi)
+        if (!exist) {
+          const data = {
+            nip: kantor.kdlokasi,
+            instansi: kantor.kdlokasi,
+            nama_lengkap: kantor.nmlokasi,
+            kode_lokasi: null,
+            kode_jabatan: null,
+            nama_jabatan: null,
+            kode_eselon: null,
+            golongan: null,
+            password: await Hash.make(kantor.kdlokasi.toString()),
+            level: 1,
+            akses: 'administrator',
+            status: 1,
+            keyword: ''
+          }
+  
+          const insert = await Login.create(data)
 
-        const insert = await Login.create(data)
-        console.log('Admin ' + insert.nama_lengkap + ' -> Success.')
+          console.log('Admin ' + insert.nama_lengkap + ' -> Created.')
+        } else {
+          exist.nama_lengkap = kantor.nmlokasi
+          await exist.save()
+
+          console.log('Admin ' + exist.nama_lengkap + ' -> Updated.')
+        }
 
         next()
       }, () => {
